9359d1195417201369e6de244006971f102dd9bd
sardiraymond <74903069+sardiraymond@users.noreply.github.com>
2023-03-03 22:02:57 +0700

    tugas add to cart demo QA

---
+++ src/main/java/com/juaracoding/selenium/googleCalculator.java
@@ -0,0 +1,9 @@
+package com.juaracoding.selenium;
+
+
+public class googleCalculator {
+    private AndroidDriver<WebElement> driver;
+
+
+
+}
---
+++ src/test/java/com/juaracoding/selenium/testCalculator.java
@@ -0,0 +1,34 @@
+package com.juaracoding.selenium;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class testCalculator {
+
+    private static AndroidDriver<MobileElement> driver;
+    private testCalculator calculator;
+
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("deviceName", "sdk_gphone_arm64");
+        capabilities.setCapability("udid", "emulator-5554");
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("platformVersion", "11.0");
+        capabilities.setCapability("appPackage", "com.google.android.calculator");
+        capabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
+
+        driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+}
+
+
